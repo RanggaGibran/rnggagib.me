65a0ffad57844f6874d85e60e98e2f914be97fb3
Rangga Gibran <rangga.gibran6555@student.unri.ac.id>
2025-05-14 19:59:53 +0700

    Update reviews.js

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -2,7 +2,7 @@
 
 class ReviewSystem {
   constructor() {
-    this.clientId = 'YOUR_DISCORD_CLIENT_ID'; // Ganti dengan Discord Client ID Anda
+    this.clientId = '1333630246737412147'; // Ganti dengan Discord Client ID Anda
     this.redirectUri = encodeURIComponent(`${window.location.origin}/auth/discord/callback`);
     this.apiEndpoint = '/api/reviews';
     this.user = null;
